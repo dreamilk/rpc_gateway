2f8b29d10781413e7dfbf6cf215c95c77b88b8fa
dreamilk <1763443263@qq.com>
2024-04-05 20:23:15 +0800

    feat: add consul

--- main.go
+++ main.go
@@ -5,12 +5,32 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/connect"
+	"go.uber.org/zap"
+
 	"github.com/dreamilk/rpc_gateway/config"
 	"github.com/dreamilk/rpc_gateway/handler"
 	"github.com/dreamilk/rpc_gateway/log"
 )
 
 func main() {
+	ctx := context.Background()
+	// Create a Consul API client
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Error(ctx, "NewClient failed", zap.Error(err))
+		return
+	}
+	// Create an instance representing this service. "my-service" is the
+	// name of _this_ service. The service should be cleaned up via Close.
+	svc, err := connect.NewService("gateway", client)
+	if err != nil {
+		log.Error(ctx, "new service failed", zap.Error(err))
+		return
+	}
+	defer svc.Close()
+
 	fmt.Println("hello rpc_gataway")
 
 	http.HandleFunc("/", handler.ServiceGateway)
