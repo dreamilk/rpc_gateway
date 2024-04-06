9e6671e21d0f288117c9da5859a5ae323d09390a
dreamilk <1763443263@qq.com>
2024-04-06 14:22:43 +0800

    feat: add url

--- handler/handler.go
+++ handler/handler.go
@@ -1,20 +1,59 @@
 package handler
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/google/uuid"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/dreamilk/rpc_gateway/log"
 	"github.com/dreamilk/rpc_gateway/utils"
 )
 
+type Response struct {
+	RetCode int         `json:"retcode"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
 func ServiceGateway(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
+	ctx, cancel := context.WithTimeout(req.Context(), 10*time.Second)
+	defer cancel()
 	ctx = utils.WithTraceId(ctx, uuid.NewString())
 
-	log.Infof(ctx, "host:%s %s", req.Host, utils.TraceId(ctx))
+	// check url valid
+	var input proto.Message
+	var output proto.Message
 
-	fmt.Fprint(w, "hello gw")
+	// invoke rpc
+	url := ""
+	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Error(ctx, "", zap.Error(err))
+	}
+
+	rpcPath := ""
+
+	err = conn.Invoke(ctx, rpcPath, input, output)
+	if err != nil {
+		log.Error(ctx, "", zap.Error(err))
+	}
+
+	b, err := json.Marshal(output)
+	if err != nil {
+		log.Error(ctx, "", zap.Error(err))
+	}
+
+	// send response
+
+	log.Infof(ctx, "host:%s %s", req.Host, utils.TraceId(ctx))
+	fmt.Fprint(w, string(b))
+
 }
