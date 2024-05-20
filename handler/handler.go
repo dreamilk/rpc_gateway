edbe4d8c5b7d53e07f6f356cfe11e2333e79e683
dreamilk <1763443263@qq.com>
2024-05-20 22:43:10 +0800

    feat: 服务发现

--- handler/handler.go
+++ handler/handler.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/hashicorp/consul/api"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -23,20 +25,49 @@ type Response struct {
 	Data    interface{} `json:"data"`
 }
 
+func searchService(ctx context.Context, serviceName string, tag string) (string, error) {
+	// Create a Consul API client
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Error(ctx, "NewClient failed", zap.Error(err))
+		return "", err
+	}
+
+	svc, _, err := client.Health().Service(serviceName, tag, true, nil)
+	if err != nil {
+		log.Error(ctx, "", zap.Error(err))
+		return "", err
+	}
+
+	if len(svc) == 0 {
+		return "", fmt.Errorf("not found %s in cousul", serviceName)
+	}
+
+	// TODO
+	// load balance
+	addr := svc[0].Node.Address + ":" + strconv.Itoa(svc[0].Service.Port)
+	return addr, nil
+}
+
 func ServiceGateway(w http.ResponseWriter, req *http.Request) {
 	ctx, cancel := context.WithTimeout(req.Context(), 10*time.Second)
 	defer cancel()
 	ctx = utils.WithTraceId(ctx, uuid.NewString())
+
+	addr, err := searchService(ctx, "consul", "")
+	if err != nil {
+		log.Error(ctx, "", zap.Error(err))
+	}
+	log.Info(ctx, "addr", zap.Any("addr", addr))
 
 	// check url valid
 	var input proto.Message
 	var output proto.Message
 
 	// invoke rpc
-	url := ""
-	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Error(ctx, "", zap.Error(err))
+		log.Error(ctx, "grpc.Dial failed", zap.Error(err))
 	}
 
 	rpcPath := ""
--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/hashicorp/consul/api"
-	"github.com/hashicorp/consul/connect"
 	"go.uber.org/zap"
 
 	"github.com/dreamilk/rpc_gateway/config"
@@ -15,22 +13,6 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
-	// Create a Consul API client
-	client, err := api.NewClient(api.DefaultConfig())
-	if err != nil {
-		log.Error(ctx, "NewClient failed", zap.Error(err))
-		return
-	}
-	// Create an instance representing this service. "my-service" is the
-	// name of _this_ service. The service should be cleaned up via Close.
-	svc, err := connect.NewService("gateway", client)
-	if err != nil {
-		log.Error(ctx, "new service failed", zap.Error(err))
-		return
-	}
-	defer svc.Close()
-
 	fmt.Println("hello rpc_gataway")
 
 	http.HandleFunc("/", handler.ServiceGateway)
